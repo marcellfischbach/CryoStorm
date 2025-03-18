813f3edae41d80b42915834e39d945bdd53fa8d9
mcel <marcellfischbach@googlemail.com>
2025-03-18 16:55:35 +0100

    Library loading from Java.

--- JCryoStorm/CsCore/src/main/java/org/cryo/core/CsCoreLibrary.java
+++ JCryoStorm/CsCore/src/main/java/org/cryo/core/CsCoreLibrary.java
@@ -1,14 +1,140 @@
 package org.cryo.core;
 
-public class CsCoreLibrary {
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
-    public static void load() {
-        System.loadLibrary("csCore");
-    }
+public class CsCoreLibrary
+{
+
+	private static final String LIBRARY_PATH = System.getProperty("user.dir");
+
+	private static final String[] LIBRARY_NAMES = new String[]{
+			"csAssimpLoader",
+			"csBullet",
+			"csCore",
+			"csCryoFile",
+			"csGame",
+			"csImgLoader",
+			"csOpenGL",
+			"csXml"
+	};
 
 
-    private CsCoreLibrary() {
+	public static void load()
+	{
+		try
+		{
+			extractLibraries();
 
-    }
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+				removeLibraries();
+				System.out.println("Custom shutdown hook is running...");
+				// Add your cleanup code here
+			}));
+
+			System.load(LIBRARY_PATH + "/" + System.mapLibraryName("csCore"));
+		}
+		catch (UnsatisfiedLinkError le)
+		{
+			System.err.println("Unable to load csCore: " + System.mapLibraryName("csCore"));
+			throw le;
+
+		}
+		catch (Exception e)
+		{
+			System.err.println("Unable to load csCore: " + System.mapLibraryName("csCore"));
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static void extractLibraries() throws Exception
+	{
+		File path = new File(LIBRARY_PATH);
+		Files.createDirectories(path.toPath());
+		for (String libraryName : LIBRARY_NAMES)
+		{
+			extractLibrary(libraryName, path);
+
+		}
+	}
+
+
+	private static void extractLibrary(String libName, File path) throws Exception
+	{
+		String nativeLibName = System.mapLibraryName(libName);
+		InputStream dllStream = CsCoreLibrary.class.getResourceAsStream("/natives/" + nativeLibName);
+
+		System.out.println("Extract '" + nativeLibName + "' to " + path);
+
+		File tmpDllFile = new File(path + "/" + nativeLibName);
+		Path tempDll = tmpDllFile.toPath();
+		Files.copy(dllStream, tempDll, StandardCopyOption.REPLACE_EXISTING);
+		dllStream.close();
+
+
+		tmpDllFile.deleteOnExit();
+
+	}
+
+
+	private static void removeLibraries()
+	{
+		File path = new File(LIBRARY_PATH);
+		for (String libraryName : LIBRARY_NAMES)
+		{
+			removeLibrary(libraryName, path);
+		}
+	}
+
+	private static void removeLibrary(String libName, File path)
+	{
+		try
+		{
+			String nativeLibName = System.mapLibraryName(libName);
+			File tmpDllFile = new File(path + "/" + nativeLibName);
+
+			tmpDllFile.deleteOnExit();
+		}
+		catch (Throwable th)
+		{
+			th.printStackTrace();
+		}
+	}
+
+	private static void markForDeletion(File file)
+	{
+		if (file.isDirectory())
+		{
+			// Recursively mark all files and subdirectories
+			for (File child : file.listFiles())
+			{
+				markForDeletion(child);
+			}
+		}
+		// Mark the file or directory for deletion on exit
+		file.deleteOnExit();
+	}
+
+	private static void deleteDirectory(File file)
+	{
+		if (file.isDirectory())
+		{
+			// Recursively delete all files and subdirectories
+			for (File child : file.listFiles())
+			{
+				deleteDirectory(child);
+			}
+		}
+		// Delete the file or empty directory
+		file.delete();
+	}
+
+	private CsCoreLibrary()
+	{
+
+	}
 
 }
--- JCryoStorm/Demo/src/main/java/org/cryo/demo/Main.java
+++ JCryoStorm/Demo/src/main/java/org/cryo/demo/Main.java
@@ -14,122 +14,136 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class Main
+{
 
 
-    private static String[] enrich (String[] args) {
-        List<String> newArgs = new ArrayList<String>();
-        boolean haveProfile = false;
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("--glProfile") && i+1 < args.length) {
-                newArgs.add(args[i]);
-                newArgs.add("compat");
-                i++;
-                haveProfile = true;
-            }
-            else {
-                newArgs.add(args[i]);
-            }
-        }
-        if (!haveProfile) {
-            newArgs.add("--glProfile");
-            newArgs.add("compat");
-        }
-        return newArgs.toArray(new String[0]);
-    }
+	private static String[] enrich(String[] args)
+	{
+		List<String> newArgs = new ArrayList<String>();
+		boolean haveProfile = false;
+		for (int i = 0; i < args.length; i++)
+		{
+			if (args[i].equals("--glProfile") && i + 1 < args.length)
+			{
+				newArgs.add(args[i]);
+				newArgs.add("compat");
+				i++;
+				haveProfile = true;
+			}
+			else
+			{
+				newArgs.add(args[i]);
+			}
+		}
+		if (!haveProfile)
+		{
+			newArgs.add("--glProfile");
+			newArgs.add("compat");
+		}
+		return newArgs.toArray(new String[0]);
+	}
 
-    public static void main(String[] origArgs) {
-        final String[] args = enrich(origArgs);
-        SwingUtilities.invokeLater(() -> {
-            CsCoreLibrary.load();
+	public static void main(String[] origArgs)
+	{
+		final String[] args = enrich(origArgs);
+		SwingUtilities.invokeLater(() -> {
+			CsCoreLibrary.load();
 
-            String rootPath = "F:\\DEV\\C++\\CryoStormDemoData\\data";
-            String configFileName = "vfs_java.config";
+			String rootPath = "F:\\DEV\\C++\\CryoStormDemoData\\data";
+			String configFileName = "vfs_java.config";
 
-            for (int i = 0; i < args.length; i++) {
-                if (args[i].equals("--data") && args.length > (i + 1)) {
-                    rootPath = args[++i];
-                }
-                if (args[i].equals("--config") && args.length > (i + 1)) {
-                    configFileName = args[++i];
-                }
-            }
+			for (int i = 0; i < args.length; i++)
+			{
+				if (args[i].equals("--data") && args.length > (i + 1))
+				{
+					rootPath = args[++i];
+				}
+				if (args[i].equals("--config") && args.length > (i + 1))
+				{
+					configFileName = args[++i];
+				}
+			}
 
-            VFS.instance().setRootPath(rootPath);
-            VFSConfigReader.read(rootPath, configFileName);
+			VFS.instance().setRootPath(rootPath);
+			VFSConfigReader.read(rootPath, configFileName);
 
-            // this causes on c++ side to initialize the reflection system
-            // without this call no connection between java and c++ is handled
-            Engine.get();
+			// this causes on c++ side to initialize the reflection system
+			// without this call no connection between java and c++ is handled
+			Engine.get();
 
-            AwtGlCanvas shared = null;
-            {
-                JFrame frame = new JFrame();
-                frame.setLayout(new BorderLayout());
-                AwtGlCanvas testCanvas = new AwtGlCanvas(shared,
-                                                         () -> initializeEngine(args),
-                                                         Main::canvasInitialized);
-                testCanvas.name = "Red";
-                frame.add(testCanvas, BorderLayout.CENTER);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(1024, 768);
-                frame.setLocation(-1100, 100);
-                frame.setVisible(true);
+			AwtGlCanvas shared = null;
+			{
+				JFrame frame = new JFrame();
+				frame.setLayout(new BorderLayout());
+				AwtGlCanvas testCanvas = new AwtGlCanvas(shared, () -> initializeEngine(args), Main::canvasInitialized);
+				testCanvas.name = "Red";
+				frame.add(testCanvas, BorderLayout.CENTER);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setSize(1024, 768);
+				frame.setLocation(-1100, 100);
+				frame.setVisible(true);
 
-                testCanvas.startRenderLoop();
-                shared = testCanvas;
-            }
+				testCanvas.startRenderLoop();
+				shared = testCanvas;
+			}
 
-            {
-                JFrame frame = new JFrame();
-                frame.setLayout(new BorderLayout());
-                AwtGlCanvas testCanvas = new AwtGlCanvas(shared,
-                                                         () -> {
-                                                         },
-                                                         Main::canvasInitialized);
-                testCanvas.name = "Red";
-                frame.add(testCanvas, BorderLayout.CENTER);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(1024, 768);
-                frame.setLocation(-2400, 100);
-                frame.setVisible(true);
+			{
+				JFrame frame = new JFrame();
+				frame.setLayout(new BorderLayout());
+				AwtGlCanvas testCanvas = new AwtGlCanvas(shared, () -> {
+				}, Main::canvasInitialized);
+				testCanvas.name = "Red";
+				frame.add(testCanvas, BorderLayout.CENTER);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setSize(1024, 768);
+				frame.setLocation(-2400, 100);
+				frame.setVisible(true);
 
-                testCanvas.startRenderLoop();
-            }
+				testCanvas.startRenderLoop();
+			}
 
-        });
-    }
-
-
-    private static World world = null;
-    private static void canvasInitialized(AwtGlCanvas canvas) {
-        if (world == null) {
-            world = new World();
-            canvas.getViewport().setWorld(world);
-            DemoWorld.create(world);
-        }
-        else {
-            canvas.getViewport().setWorld(world);
-        }
-    }
+		});
+	}
 
 
-    private static boolean initialized = false;
+	private static World world = null;
 
-    public static void initializeEngine(String[] args) {
-        if (initialized) {
-            return;
-        }
+	private static void canvasInitialized(AwtGlCanvas canvas)
+	{
+		if (world == null)
+		{
+			world = new World();
+			canvas.getViewport().setWorld(world);
+			DemoWorld.create(world);
+		}
+		else
+		{
+			canvas.getViewport().setWorld(world);
+		}
+	}
 
-        ModuleConfig moduleConfig = new ModuleConfig();
-        if (!moduleConfig.loadModuleConfig()) {
-            System.out.println("Failed to load module config");
-            return;
-        }
 
-        Engine.get().initializeEngine(args, moduleConfig);
+	private static boolean initialized = false;
 
-        initialized = true;
+	public static void initializeEngine(String[] args)
+	{
+		if (initialized)
+		{
+			return;
+		}
 
-    }
+		ModuleConfig moduleConfig = new ModuleConfig();
+		if (!moduleConfig.loadModuleConfig())
+		{
+			System.out.println("Failed to load module config");
+			return;
+		}
+
+		Engine.get().initializeEngine(args, moduleConfig);
+
+		initialized = true;
+
+	}
+
 }
